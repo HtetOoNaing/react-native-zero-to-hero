76f6921e02115ef907c3c7d11a409fb7f12cc8ee
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-11-19 23:40:37 +0630

    text input and keyboard

--- App.js
+++ App.js
@@ -13,45 +13,21 @@ import {
 	ScrollView,
 	StyleSheet,
 	Text,
+	TextInput,
 	View,
 } from 'react-native';
 
 const App = () => {
-	const [items, setItems] = useState([
-		{ name: 'Item 1' },
-		{ name: 'Item 2' },
-		{ name: 'Item 3' },
-		{ name: 'Item 4' },
-		{ name: 'Item 5' },
-		{ name: 'Item 6' },
-		{ name: 'Item 7' },
-		{ name: 'Item 8' },
-		{ name: 'Item 9' },
-		{ name: 'Item 10' },
-	])
-	const [refreshing, setRefreshing] = useState(false);
 
-	const onRefresh = () => {
-		setRefreshing(true);
-		setItems([...items, { name: `Item ${items.length + 1}` }]);
-		setRefreshing(false);
-	}
+	const [name, setName] = useState('')
 
 	return (
 		<View style={styles.body}>
-			<FlatList keyExtractor={(item, index) => index.toString()} data={items} renderItem={({item}) => (
-				<View key={item.key} style={styles.item}>
-					<Text style={styles.text}>{item.name}</Text>
-				</View>
-			)}
-			refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} colors={['#ff00ff']} />} />
-			{/* <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} colors={['#ff00ff']} />}>
-				{items.map(item => (
-					<View key={item.key} style={styles.item}>
-						<Text style={styles.text}>{item.name}</Text>
-					</View>
-				))}
-			</ScrollView> */}
+			<Text style={styles.text}>
+				Please write your name: 
+			</Text>
+			<TextInput style={styles.input} placeholder="e.g John Doe" onChangeText={(value) => setName(value)} secureTextEntry={true} />
+			<Text style={styles.text}>Your name is : {name}</Text>
 		</View>
 	);
 };
@@ -59,21 +35,21 @@ const App = () => {
 const styles = StyleSheet.create({
 	body: {
 		flex: 1,
-		flexDirection: 'column',
+		alignItems: 'center',
 		backgroundColor: '#fff',
 		marginTop: 35
 	},
-	item: {
-		backgroundColor: '#4ae1fa',
-		justifyContent: 'center',
-		alignItems: 'center',
-		margin: 10
-	},
 	text: {
 		color: '#000',
-		fontSize: 45,
-		fontStyle: 'italic',
+		fontSize: 20,
 		margin: 10
+	},
+	input: {
+		width: 200,
+		borderWidth: 1,
+		borderColor: '#555',
+		borderRadius: 5,
+		padding: 10,
 	}
 });
 
