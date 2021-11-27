1daeb7fc030cecf29acd44e7d985b793a55bd6d0
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-11-28 01:04:12 +0630

    add icon to drawer tab

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ScreenB from './ScreenB';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const Drawer = createDrawerNavigator();
 
@@ -11,8 +12,8 @@ const App = () => {
 	return (
 		<NavigationContainer>
 			<Drawer.Navigator>
-				<Drawer.Screen name="Screen_A" component={ScreenA} options={{ title: 'Screen A Title'}} />
-				<Drawer.Screen name="Screen_B" component={ScreenB} options={{ title: 'Screen B Title'}} />
+				<Drawer.Screen name="Screen_A" component={ScreenA} options={{ title: 'Screen A Title', drawerIcon: ({ focused }) => (<FontAwesome5 name="autoprefixer" size={focused ? 25 : 20} color={focused ? '#0080ff' : '#999999'} />) }} />
+				<Drawer.Screen name="Screen_B" component={ScreenB} options={{ title: 'Screen B Title', drawerIcon: ({ focused }) => (<FontAwesome5 name="btc" size={focused ? 25 : 20} color={focused ? '#0080ff' : '#999999'} />) }} />
 			</Drawer.Navigator>
 		</NavigationContainer>
 	);
