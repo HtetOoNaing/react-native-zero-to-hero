ec3ef6eda06dbc0e2538ea1d5e8f3f6aec2bfc40
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-11-27 15:23:41 +0630

    stack navigator done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator()
 
