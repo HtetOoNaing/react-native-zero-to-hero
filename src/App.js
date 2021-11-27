bbcdf11725fc0b69f450299d35baa8093cec2770
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-11-28 00:29:11 +0630

    add deawer navigation

--- src/App.js
+++ src/App.js
@@ -3,34 +3,18 @@ import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
 import { NavigationContainer } from '@react-navigation/native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome5';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const Tab = createMaterialTopTabNavigator()
+const Drawer = createDrawerNavigator();
 
 const App = () => {
 	return (
 		<NavigationContainer>
-			<Tab.Navigator screenOptions={({ route }) => ({
-				tabBarIcon: ({ focused, size, color }) => {
-					let iconName;
-					if (route.name === 'Screen_A') {
-						iconName = "autoprefixer"
-					} else if (route.name === 'Screen_B') {
-						iconName = "btc"
-					}
-					return <FontAwesome name={iconName} size={size} color={color} />
-				},
-				tabBarActiveTintColor: '#f0f',
-				tabBarShowIcon: true,
-				tabBarStyle: {marginTop: 30}
-			})}
-			activeColor="#f0edf6"
-			inactiveColor="#3e2465"
-			barStyle={{backgroundColor: "#694fad"}}
-			>
-				<Tab.Screen name="Screen_A" component={ScreenA} />
-				<Tab.Screen name="Screen_B" component={ScreenB} />
-			</Tab.Navigator>
+			<Drawer.Navigator>
+				<Drawer.Screen name="Screen_A" component={ScreenA} />
+				<Drawer.Screen name="Screen_B" component={ScreenB} />
+			</Drawer.Navigator>
 		</NavigationContainer>
 	);
 };
