ce5ab9196691357c1d2865856ee00e24983a51a1
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-11-28 00:45:36 +0630

    show screen title different from screen name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
-import { NavigationContainer } from '@react-navigation/native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome5';
 import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Drawer = createDrawerNavigator();
@@ -11,9 +10,9 @@ const Drawer = createDrawerNavigator();
 const App = () => {
 	return (
 		<NavigationContainer>
-			<Drawer.Navigator screenOptions={{ drawerPosition: 'right' }}>
-				<Drawer.Screen name="Screen_A" component={ScreenA} />
-				<Drawer.Screen name="Screen_B" component={ScreenB} />
+			<Drawer.Navigator>
+				<Drawer.Screen name="Screen_A" component={ScreenA} options={{ title: 'Screen A Title'}} />
+				<Drawer.Screen name="Screen_B" component={ScreenB} options={{ title: 'Screen B Title'}} />
 			</Drawer.Navigator>
 		</NavigationContainer>
 	);
