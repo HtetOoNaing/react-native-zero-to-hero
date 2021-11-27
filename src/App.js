230401b1c0d4ea6eee2e5eab4e33978df5406f1a
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-11-28 00:18:37 +0630

    refactor the code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
 import { NavigationContainer } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FontAwesome from 'react-native-vector-icons/FontAwesome5';
-// import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-// const Tab = createBottomTabNavigator()
-// const Tab = createMaterialBottomTabNavigator();
 const Tab = createMaterialTopTabNavigator()
 
 const App = () => {
@@ -22,13 +18,9 @@ const App = () => {
 					} else if (route.name === 'Screen_B') {
 						iconName = "btc"
 					}
-					// size = focused ? 25 : 20;
-					// color = focused ? '#f0f' : '#555';
 					return <FontAwesome name={iconName} size={size} color={color} />
 				},
 				tabBarActiveTintColor: '#f0f',
-				// tabBarInactiveTintColor: '#555',
-				// tabBarActiveBackgroundColor: '#ccc',
 				tabBarShowIcon: true,
 				tabBarStyle: {marginTop: 30}
 			})}
