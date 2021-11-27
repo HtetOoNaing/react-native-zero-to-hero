83deb9bcba7116674041db5c8f2cce6bebdeb320
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-11-27 22:18:41 +0630

    add fontawesome5 icon to tabbar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome from 'react-native-vector-icons/FontAwesome5';
 
 const Tab = createBottomTabNavigator()
 
 const App = () => {
 	return (
 		<NavigationContainer>
-			<Tab.Navigator>
+		<Tab.Navigator screenOptions={({route}) => ({
+			tabBarIcon: ({focused, size, color}) => {
+				let iconName;
+				if(route.name === 'Screen_A') {
+					iconName = "autoprefixer"
+				} else if (route.name === 'Screen_B') {
+					iconName = "btc"
+				}
+				return <FontAwesome name={iconName} size={30} color="#900" />
+			}
+		})}>
 				<Tab.Screen name="Screen_A" component={ScreenA} />
 				<Tab.Screen name="Screen_B" component={ScreenB} />
 			</Tab.Navigator>
