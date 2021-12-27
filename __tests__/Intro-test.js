100cc7edb302d6fb40829e842cb50d7157271295
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-12-27 12:42:59 +0630

    add flatlist component testing with jest -u

--- __tests__/Intro-test.js
+++ __tests__/Intro-test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FlatList, Text } from 'react-native';
 import renderer from 'react-test-renderer';
 import Intro from '../src/utils/Intro';
 
@@ -6,3 +7,8 @@ test('renders correctly', () => {
   const tree = renderer.create(<Intro />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+it('renders the FlatList component', () => {
+	const tree = renderer.create(<FlatList data={['Item1', 'Item2', 'Item3']} keyExtractor={item => item} renderItem={({item}) => <Text>{item}</Text> } />).toJSON();
+	expect(tree).toMatchSnapshot();
+})
