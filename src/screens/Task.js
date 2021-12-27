51b284bd813546c8c62459ba73e6283b1fd6d2e9
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-12-27 20:05:34 +0630

    add add task action

--- src/screens/Task.js
+++ src/screens/Task.js
@@ -1,16 +1,45 @@
+import AsyncStorageLib from '@react-native-async-storage/async-storage'
 import React, { useState } from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
+import { Alert, StyleSheet, TextInput, View } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
+import { setTasks } from '../redux/actions'
 import CustomButton from '../utils/CustomButton'
 
-const Task = () => {
+const Task = ({ navigation }) => {
+
+	const { tasks, taskID } = useSelector(state => state.userReducer)
+	const dispatch = useDispatch()
+
 	const [title, setTitle] = useState('');
 	const [description, setDescription] = useState('');
 
+	const handleTask = () => {
+		if (title.length === 0) {
+			Alert.alert('Warning!', 'Please write your task title')
+		} else {
+			try {
+				let Task = {
+					ID: taskID,
+					Title: title,
+					Description: description
+				}
+				let newTasks = [...tasks, Task]
+				AsyncStorageLib.setItem('Tasks', JSON.stringify(newTasks)).then(() => {
+					dispatch(setTasks(newTasks));
+					Alert.alert('Success!', 'Task saved successfully ')
+					navigation.goBack()
+				}).catch(err => console.log(err))
+			} catch (error) {
+				console.log(error)
+			}
+		}
+	}
+
 	return (
 		<View style={styles.body}>
-			<TextInput style={styles.input} placeholder='Title' value={title} onPressHandler={(value) => setTitle(value)} />
-			<TextInput style={styles.input} placeholder='Description' multiline value={description} onPressHandler={(value) => setDescription(value)} />
-			<CustomButton title="Save Task" style={{ width: '100%', marginTop: 10 }} />
+			<TextInput style={styles.input} placeholder='Title' value={title} onChangeText={(value) => setTitle(value)} />
+			<TextInput style={styles.input} placeholder='Description' multiline value={description} onChangeText={(value) => setDescription(value)} />
+			<CustomButton title="Save Task" style={{ width: '100%', marginTop: 10 }} onPressHandler={handleTask} />
 		</View>
 	)
 }
