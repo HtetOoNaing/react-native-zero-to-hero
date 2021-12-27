3a58ea9e9a9adcc974e0da3bb70310cd88b8b5a4
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-12-27 19:25:29 +0630

    design task form

--- src/screens/Task.js
+++ src/screens/Task.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, TextInput, View } from 'react-native'
+import CustomButton from '../utils/CustomButton'
 
 const Task = () => {
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+
 	return (
 		<View style={styles.body}>
-			<Text>Task</Text>
+			<TextInput style={styles.input} placeholder='Title' value={title} onPressHandler={(value) => setTitle(value)} />
+			<TextInput style={styles.input} placeholder='Description' multiline value={description} onPressHandler={(value) => setDescription(value)} />
+			<CustomButton title="Save Task" style={{ width: '100%', marginTop: 10 }} />
 		</View>
 	)
 }
@@ -16,5 +22,18 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		padding: 10
+	},
+	input: {
+		width: '100%',
+		borderWidth: 1,
+		borderColor: '#555',
+		borderRadius: 10,
+		backgroundColor: '#fff',
+		textAlign: 'left',
+		fontSize: 20,
+		margin: 10,
+		padding: 10,
+		height: 50,
+		lineHeight: 20
 	}
 })
