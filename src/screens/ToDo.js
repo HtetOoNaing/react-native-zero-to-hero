cf72f185324638a1528adcf872f15a02bdfc97ed
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-12-27 23:05:14 +0630

    add delete action on each task

--- src/screens/ToDo.js
+++ src/screens/ToDo.js
@@ -1,6 +1,6 @@
 import AsyncStorageLib from '@react-native-async-storage/async-storage'
 import React, { useEffect } from 'react'
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Alert, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5'
 import { useDispatch, useSelector } from 'react-redux'
 import { setTaskID, setTasks } from '../redux/actions'
@@ -17,21 +17,34 @@ const ToDo = ({ navigation }) => {
 	const getTasks = () => {
 		AsyncStorageLib.getItem('Tasks').then(tasks => {
 			const parsedTasks = JSON.parse(tasks)
-			if(parsedTasks && typeof parsedTasks === 'object') {
+			if (parsedTasks && typeof parsedTasks === 'object') {
 				dispatch(setTasks(parsedTasks))
 			}
 		}).catch(err => console.log(err))
 	}
 
+	const deleteTask = (id) => {
+		const filteredTasks = tasks.filter(task => task.ID !== id);
+		AsyncStorageLib.setItem('Tasks', JSON.stringify(filteredTasks)).then(() => {
+			dispatch(setTasks(filteredTasks));
+			Alert.alert('Success!', 'Task removed successfully.')
+		}).catch(err => console.log(err))
+	}
+
 	return (
 		<View style={styles.body}>
-			<FlatList data={tasks} renderItem={({item}) => (
+			<FlatList data={tasks} renderItem={({ item }) => (
 				<TouchableOpacity onPress={() => {
 					dispatch(setTaskID(item.ID))
 					navigation.navigate('Task')
 				}} style={styles.item}>
-					<Text style={[styles.title, GlobalStyle.CustomFont]} numberOfLines={1}>{item.Title}</Text>
-					<Text style={styles.subtitle} numberOfLines={1}>{item.Description}</Text>
+					<View style={styles.item_row}>
+						<View style={styles.item_body}>
+							<Text style={[styles.title, GlobalStyle.CustomFont]} numberOfLines={1}>{item.Title}</Text>
+							<Text style={styles.subtitle} numberOfLines={1}>{item.Description}</Text>
+						</View>
+						<TouchableOpacity onPress={() => deleteTask(item.ID)} style={styles.delete}><FontAwesome5Icon name="trash" size={25} color="#ff3636" /></TouchableOpacity>
+					</View>
 				</TouchableOpacity>
 			)} keyExtractor={(item, index) => index.toString()} />
 			<TouchableOpacity onPress={() => {
@@ -60,6 +73,13 @@ const styles = StyleSheet.create({
 		right: 10,
 		elevation: 5
 	},
+	item_row: {
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	item_body: {
+		flex: 1
+	},
 	item: {
 		marginHorizontal: 10,
 		marginVertical: 7,
@@ -78,6 +98,12 @@ const styles = StyleSheet.create({
 		color: '#999',
 		fontSize: 20,
 		margin: 5
+	},
+	delete: {
+		width: 50,
+		height: 50,
+		justifyContent: 'center',
+		alignItems: 'center'
 	}
 })
 
