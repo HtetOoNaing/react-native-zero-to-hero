262528f244fe84b7b2789151955c88d4577226b0
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-12-27 18:44:07 +0630

    add plus button on ToDo

--- src/screens/ToDo.js
+++ src/screens/ToDo.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5'
 
 const ToDo = () => {
 	return (
-		<View>
-			<Text>ToDo</Text>
+		<View style={styles.body}>
+			<TouchableOpacity style={styles.button}>
+				<FontAwesome5Icon name='plus' size={20} color="#fff" />
+			</TouchableOpacity>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	body: {
+		flex: 1
+	},
+	button: {
+		width: 60,
+		height: 60,
+		borderRadius: 30,
+		backgroundColor: '#0080ff',
+		justifyContent: 'center',
+		alignItems: 'center',
+		position: 'absolute',
+		bottom: 10,
+		right: 10,
+		elevation: 5
+	}
+})
 
 export default ToDo
