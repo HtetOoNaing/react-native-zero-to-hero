5f63819cacd6fc19767404e53baa05f3cb5cfc2b
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-12-27 22:38:29 +0630

    add tasks list on todo screen

--- src/screens/ToDo.js
+++ src/screens/ToDo.js
@@ -1,9 +1,10 @@
 import AsyncStorageLib from '@react-native-async-storage/async-storage'
 import React, { useEffect } from 'react'
-import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5'
 import { useDispatch, useSelector } from 'react-redux'
 import { setTaskID, setTasks } from '../redux/actions'
+import GlobalStyle from '../utils/GlobalStyle'
 
 const ToDo = ({ navigation }) => {
 	const { tasks } = useSelector(state => state.userReducer)
@@ -24,6 +25,12 @@ const ToDo = ({ navigation }) => {
 
 	return (
 		<View style={styles.body}>
+			<FlatList data={tasks} renderItem={({item}) => (
+				<TouchableOpacity style={styles.item}>
+					<Text style={[styles.title, GlobalStyle.CustomFont]} numberOfLines={1}>{item.Title}</Text>
+					<Text style={styles.subtitle} numberOfLines={1}>{item.Description}</Text>
+				</TouchableOpacity>
+			)} keyExtractor={(item, index) => index.toString()} />
 			<TouchableOpacity onPress={() => {
 				dispatch(setTaskID(tasks.length + 1))
 				navigation.navigate('Task')
@@ -49,6 +56,25 @@ const styles = StyleSheet.create({
 		bottom: 10,
 		right: 10,
 		elevation: 5
+	},
+	item: {
+		marginHorizontal: 10,
+		marginVertical: 7,
+		paddingHorizontal: 10,
+		backgroundColor: '#fff',
+		justifyContent: 'center',
+		borderRadius: 10,
+		elevation: 5
+	},
+	title: {
+		color: '#000',
+		fontSize: 30,
+		margin: 5
+	},
+	subtitle: {
+		color: '#999',
+		fontSize: 20,
+		margin: 5
 	}
 })
 
